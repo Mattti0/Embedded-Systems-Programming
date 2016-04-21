30eb9150f2d835270496b3e1cbfc7d35df805c21
Matti Suominen <matsuominen@gmail.com>
2016-04-21 08:08:45 +0300

    Small fixes
    Added client make to Makefile
    Cleared commented lines

--- rpi-iec/src/iec-server.c
+++ rpi-iec/src/iec-server.c
@@ -17,7 +17,8 @@
 #include "static_model.h"
 #include "../../rpi-mysql/mysql_info.h"
 
-typedef struct {
+typedef struct
+{
 	DataAttribute *p_attr;
 	DataAttribute *p_ts;
 	float value;
@@ -27,13 +28,14 @@ typedef struct {
 
 static iecValue_t values[4];
 
+static void * objects[4] =
+{ IEDMODEL_VaconFreqConverter_DSFC1_AnIn0,
+IEDMODEL_VaconFreqConverter_DSFC1_AnIn1,
+IEDMODEL_VaconFreqConverter_DSFC1_AnIn2,
+IEDMODEL_VaconFreqConverter_DSFC1_AnIn3 };
 
-static void * objects[4] = {IEDMODEL_VaconFreqConverter_DSFC1_AnIn0,
-					IEDMODEL_VaconFreqConverter_DSFC1_AnIn1,
-					IEDMODEL_VaconFreqConverter_DSFC1_AnIn2,
-					IEDMODEL_VaconFreqConverter_DSFC1_AnIn3};
-
-static const char *names[4] = {"AnIn0", "AnIn1", "AnIn2", "AnIn3"};
+static const char *names[4] =
+{ "AnIn0", "AnIn1", "AnIn2", "AnIn3" };
 
 /* import IEC 61850 device model created from SCL-File */
 extern IedModel iedModel;
@@ -42,13 +44,10 @@ static int running = 0;
 static IedServer iedServer = NULL;
 
 MYSQL *my_con;
-MYSQL_RES *result;
-MYSQL_ROW row;
 
-void
-sigint_handler(int signalId)
+void sigint_handler(int signalId)
 {
-    running = 0;
+	running = 0;
 }
 
 void getValuesFromDatabase()
@@ -57,139 +56,137 @@ void getValuesFromDatabase()
 	char query[256];
 	int column;
 	int ret;
-	MYSQL_ROW row_temp;
+	MYSQL_RES *result;
 	MYSQL_RES *res_temp;
+	MYSQL_ROW row;
+	MYSQL_ROW row_temp;
 
-for (i = 0; i < 4; i++)
-{
-	sprintf(query, "SELECT id FROM %s WHERE iec_name = '%s'", idTable, names[i]);
-	if (!mysql_query(my_con, query))
+	for (i = 0; i < 4; i++)
 	{
+		sprintf(query, "SELECT id FROM %s WHERE iec_name = '%s'", idTable,
+				names[i]);
+		if (!mysql_query(my_con, query))
+		{
 			result = mysql_store_result(my_con);
 			row_temp = mysql_fetch_row(result);
-			sprintf(query, "SELECT Value, Time, Validness FROM %s WHERE id = %d", valueTable, atoi(row_temp[0]));
+			sprintf(query,
+					"SELECT Value, Time, Validness FROM %s WHERE id = %d",
+					valueTable, atoi(row_temp[0]));
 			if (!mysql_query(my_con, query))
 			{
 				res_temp = mysql_store_result(my_con);
 				row = mysql_fetch_row(res_temp);
 				values[i].value = atof(row[0]);
 				Timestamp_setTimeInSeconds(&values[i].ts, atol(row[1]));
-				if(!strcmp(row[2], "True"))
+				if (!strcmp(row[2], "True"))
 					values[i].validity = 1;
 				else
 					values[i].validity = 0;
 			}
 
+		}
+		else
+			printf("query failed\n");
 	}
-	else
-		printf("query failed\n");
-}
-
-}
-
-
-void getTimestamps(Timestamp *ts)
-{
-	int i;
-    uint64_t timestamp = Hal_getTimeInMs();
-
-    for(i = 0; i < 10; i++)
-    {
-        Timestamp_clearFlags(ts+i);
-        Timestamp_setTimeInMilliseconds(ts+i, timestamp);
-        Timestamp_setLeapSecondKnown(ts+i, true);
-    }
 
 }
 
 void updateValues()
 {
 	int i;
-    IedServer_lockDataModel(iedServer);
-    for (i = 0; i < 4; i++)
-    {
-    		IedServer_updateTimestampAttributeValue(iedServer, objects[i], &values[i].ts);
-        IedServer_updateFloatAttributeValue(iedServer, values[i].p_attr, values[i].value);
-    }
-    IedServer_unlockDataModel(iedServer);
+	IedServer_lockDataModel(iedServer);
+	for (i = 0; i < 4; i++)
+	{
+		IedServer_updateTimestampAttributeValue(iedServer, objects[i],
+				&values[i].ts);
+		IedServer_updateFloatAttributeValue(iedServer, values[i].p_attr,
+				values[i].value);
+	}
+	IedServer_unlockDataModel(iedServer);
 }
 
-static ControlHandlerResult
-controlHandlerForBinaryOutput(void* parameter, MmsValue* value, bool test)
+static ControlHandlerResult controlHandlerForBinaryOutput(void* parameter,
+		MmsValue* value, bool test)
 {
 	int i;
-    if (test)
-        return CONTROL_RESULT_FAILED;
+	if (test)
+		return CONTROL_RESULT_FAILED;
 
-    if (MmsValue_getType(value) == MMS_BOOLEAN) {
-        printf("received binary control command: ");
+	if (MmsValue_getType(value) == MMS_BOOLEAN)
+	{
+		printf("received binary control command: ");
 
-        if (MmsValue_getBoolean(value))
-            printf("on\n");
-        else
-            printf("off\n");
-    }
-    else
-        return CONTROL_RESULT_FAILED;
+		if (MmsValue_getBoolean(value))
+			printf("on\n");
+		else
+			printf("off\n");
+	}
+	else
+		return CONTROL_RESULT_FAILED;
 
-    uint64_t timeStamp = Hal_getTimeInMs();
+	uint64_t timeStamp = Hal_getTimeInMs();
 
-    for (i = 0; i < 4; i++)
-    {
-    	if (parameter == objects[i])
-    	{
-    		IedServer_updateUTCTimeAttributeValue(iedServer, objects[i], timeStamp);
-    		IedServer_updateAttributeValue(iedServer, values[i].p_attr, value);
-    	}
-    }
+	for (i = 0; i < 4; i++)
+	{
+		if (parameter == objects[i])
+		{
+			IedServer_updateUTCTimeAttributeValue(iedServer, objects[i],
+					timeStamp);
+			IedServer_updateAttributeValue(iedServer, values[i].p_attr, value);
+		}
+	}
 
-    return CONTROL_RESULT_OK;
+	return CONTROL_RESULT_OK;
 }
 
-
-static void
-connectionHandler (IedServer self, ClientConnection connection, bool connected, void* parameter)
+static void connectionHandler(IedServer self, ClientConnection connection,
+		bool connected, void* parameter)
 {
-    if (connected)
-        printf("Connection opened\n");
-    else
-        printf("Connection closed\n");
+	if (connected)
+		printf("Connection opened\n");
+	else
+		printf("Connection closed\n");
 }
 
-int
-main(int argc, char** argv)
+int main(int argc, char** argv)
 {
-    printf("Using libIEC61850 version %s\n", LibIEC61850_getVersionString());
+	printf("Using libIEC61850 version %s\n", LibIEC61850_getVersionString());
 
-    iedServer = IedServer_create(&iedModel);
+	iedServer = IedServer_create(&iedModel);
 
-    /* Install handler for operate command */
-    IedServer_setControlHandler(iedServer, IEDMODEL_VaconFreqConverter_DSFC1_AnIn0,
-            (ControlHandler) controlHandlerForBinaryOutput,
+	/* Install handler for operate command */
+	IedServer_setControlHandler(iedServer,
+			IEDMODEL_VaconFreqConverter_DSFC1_AnIn0,
+			(ControlHandler) controlHandlerForBinaryOutput,
 			IEDMODEL_VaconFreqConverter_DSFC1_AnIn0);
 
-    IedServer_setControlHandler(iedServer, IEDMODEL_VaconFreqConverter_DSFC1_AnIn1,
-            (ControlHandler) controlHandlerForBinaryOutput,
+	IedServer_setControlHandler(iedServer,
+			IEDMODEL_VaconFreqConverter_DSFC1_AnIn1,
+			(ControlHandler) controlHandlerForBinaryOutput,
 			IEDMODEL_VaconFreqConverter_DSFC1_AnIn1);
 
-    IedServer_setControlHandler(iedServer, IEDMODEL_VaconFreqConverter_DSFC1_AnIn2,
-            (ControlHandler) controlHandlerForBinaryOutput,
+	IedServer_setControlHandler(iedServer,
+			IEDMODEL_VaconFreqConverter_DSFC1_AnIn2,
+			(ControlHandler) controlHandlerForBinaryOutput,
 			IEDMODEL_VaconFreqConverter_DSFC1_AnIn2);
 
-    IedServer_setControlHandler(iedServer, IEDMODEL_VaconFreqConverter_DSFC1_AnIn3,
-            (ControlHandler) controlHandlerForBinaryOutput,
+	IedServer_setControlHandler(iedServer,
+			IEDMODEL_VaconFreqConverter_DSFC1_AnIn3,
+			(ControlHandler) controlHandlerForBinaryOutput,
 			IEDMODEL_VaconFreqConverter_DSFC1_AnIn3);
 
-    IedServer_setConnectionIndicationHandler(iedServer, (IedConnectionIndicationHandler) connectionHandler, NULL);
+	IedServer_setConnectionIndicationHandler(iedServer,
+			(IedConnectionIndicationHandler) connectionHandler, NULL);
 
-    /* MMS server will be instructed to start listening to client connections. */
-    IedServer_start(iedServer, 102);
+	/* MMS server will be instructed to start listening to client connections. */
+	IedServer_start(iedServer, 102);
 
-    if (!IedServer_isRunning(iedServer)) {
-        printf("Starting server failed! Exit.\n");
-        IedServer_destroy(iedServer);
-        exit(-1);
-    }
+	if (!IedServer_isRunning(iedServer))
+	{
+		printf("Starting server failed! Exit.\n");
+		IedServer_destroy(iedServer);
+		exit(-1);
+	}
 
 	my_con = mysql_init(NULL);
 	mysql_real_connect(my_con, server, my_user, my_pass, database, 0, NULL, 0);
@@ -200,27 +197,28 @@ main(int argc, char** argv)
 	values[3].p_attr = &iedModel_VaconFreqConverter_DSFC1_AnIn3_mag_f;
 
 	values[0].p_ts = &iedModel_VaconFreqConverter_DSFC1_AnIn0_t;
-	values[0].p_ts = &iedModel_VaconFreqConverter_DSFC1_AnIn1_t;
-	values[0].p_ts = &iedModel_VaconFreqConverter_DSFC1_AnIn2_t;
-	values[0].p_ts = &iedModel_VaconFreqConverter_DSFC1_AnIn3_t;
+	values[1].p_ts = &iedModel_VaconFreqConverter_DSFC1_AnIn1_t;
+	values[2].p_ts = &iedModel_VaconFreqConverter_DSFC1_AnIn2_t;
+	values[3].p_ts = &iedModel_VaconFreqConverter_DSFC1_AnIn3_t;
 
 	running = 1;
 
-    signal(SIGINT, sigint_handler);
+	signal(SIGINT, sigint_handler);
 
-    while (running) {
+	while (running)
+	{
 
-    	getValuesFromDatabase();
+		getValuesFromDatabase();
 
-    	updateValues();
+		updateValues();
 
-        Thread_sleep(100);
-    }
+		Thread_sleep(100);
+	}
 
-    /* stop MMS server - close TCP server socket and all client sockets */
-    IedServer_stop(iedServer);
+	/* stop MMS server - close TCP server socket and all client sockets */
+	IedServer_stop(iedServer);
 
-    /* Cleanup - free all resources */
-    IedServer_destroy(iedServer);
+	/* Cleanup - free all resources */
+	IedServer_destroy(iedServer);
 
 } /* main() */
--- rpi-mb/modbus_update.c
+++ rpi-mb/modbus_update.c
@@ -15,29 +15,12 @@
 #error "MySQL info not exists run rpi-mysql/initmysql.sh"
 #endif
 
-char *time_stamp(){
-
-	char *timestamp = (char *)malloc(sizeof(char) * 20);
-	time_t ltime;
-	ltime=time(NULL);
-	struct tm *tm;
-	tm=localtime(&ltime);
-
-	sprintf(timestamp,"%04d-%02d-%02d %02d:%02d:%02d", tm->tm_year+1900, tm->tm_mon,
-		tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
-	return timestamp;
-}
-
 int main(void)
 {
 	MYSQL *my_con;
 	MYSQL_RES *result;
 	MYSQL_ROW row;
 
-/*	char *my_server = "localhost";
-	char *my_user = "iec_usr";
-	char *my_pass = "iec_pass";
-	char *database = "iec_base";*/
 	my_con = mysql_init(NULL);
 	mysql_real_connect(my_con, server, my_user, my_pass, database, 0, NULL, 0);
 
@@ -46,7 +29,6 @@ int main(void)
 
 	modbus_t *mb_con;
 	uint16_t tempMBValue;
-	uint16_t tempMBAddress;
 	int ret;
 
 	mb_con = modbus_new_rtu("/dev/ttyUSB0", 9600, 'N', 8, 2);
@@ -55,7 +37,6 @@ int main(void)
 	while(1)
 	{
 		sprintf(query, "SELECT id, mb_addr FROM %s", idTable);
-		/*if(!mysql_query(my_con, "SELECT id, mb_addr FROM tIndex"))*/
 		if(!mysql_query(my_con, query))
 		{
 			result = mysql_store_result(my_con);
@@ -64,7 +45,7 @@ int main(void)
 			{
 				if((ret = modbus_read_input_registers(mb_con, atoi(row[1]), 1, &tempMBValue)) > 0)
 				{
-					sprintf(query, "UPDATE %s SET Value = %d, Time = %ld, Validness = 'TRUE' WHERE id = %d;", valueTable, tempMBValue, (long)time(NULL), atoi(row[0]));
+					sprintf(query, "UPDATE %s SET Value = %d, Time = %l, Validness = 'TRUE' WHERE id = %d;", valueTable, tempMBValue, (long)time(NULL), atoi(row[0]));
 					if(!mysql_query(my_con, query))
 					{
 					}
@@ -73,11 +54,16 @@ int main(void)
 				{
 					sprintf(query, "UPDATE %s SET Validness = 'FALSE' WHERE id = %d;", valueTable, atoi(row[0]));
 				}
+				else
+				{
+					/* Error */
+					sprintf(query, "UPDATE %s SET Validness = 'FALSE' WHERE id = %d;", valueTable, atoi(row[0]));
+				}
 			}
 		}
 		sleep(1);
 	}
-
+	modbus_close(mb_con);
 	modbus_free(mb_con);
 	mysql_free_result(result);
 	mysql_close(my_con);
